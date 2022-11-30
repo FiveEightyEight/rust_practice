4eb7412a7a91b12b9847605792863bd98c765749
Robert Abreu <robert.abreu3@gmail.com>
2022-11-30 12:05:36 -0500

    learning basic rust types

--- app.rs
+++ app.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, World!");
+
+    println!("In rust you do not need to define the type of a variable, but you can if you want to.");
+    let x: f64 = 1.1;
+    let y = 2.2;
+    println!("possible types are: f32, f64, i32, i64, u32, u64, bool, char, str, String");
+    
+    let answer = multiply_both(x, y);
+    println!("{} * {} = {}", x, y, answer);
+}
+
+fn multiply_both(x: f64, y: f64) -> f64 {
+    return x * y;
 }
