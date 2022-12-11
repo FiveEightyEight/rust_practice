05966e92409a7a4065e6f64a49627ea247f092b4
Robert Abreu <robert.abreu3@gmail.com>
2022-12-11 12:27:38 -0500

    fn test

--- app.rs
+++ app.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("In rust you do not need to define the type of a variable, but you can if you want to.");
     let x: f64 = 1.1;
     let y = 2.2;
-    println!("possible types are: f32, f64, i32, i64, u32, u64, bool, char, str, String");
+    println!("possible types are: f32, f64, i8, i16, i32, i64, i128, u32, u64, bool, char, str, String");
     
     let answer = multiply_both(x, y);
     println!("{} * {} = {}", x, y, answer);
@@ -12,4 +12,8 @@ fn main() {
 
 fn multiply_both(x: f64, y: f64) -> f64 {
     return x * y;
+}
+
+fn divide(x: i32, y: u16) -> f64 {
+    return x as f64 / y as f64;
 }
